4af220fad615c3d4f4ddbc91d33c5aac4265d0a0
Iury De Matos Souza <iury.souza@ebserh.gov.br>
2026-06-10 15:43:20 -0300

    feat: adiciona pasta final com configuracoes corretas de server.js e package.json

---
+++ final/src/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
+
+router.use(authMiddleware);
+
+router.get('/', getTasks);
+router.post('/', createTask);
+router.put('/:id', updateTask);
+router.delete('/:id', deleteTask);
+
+module.exports = router;
